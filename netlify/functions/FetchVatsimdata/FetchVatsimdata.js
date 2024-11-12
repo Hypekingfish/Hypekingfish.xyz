d982f2bf34e66e0aeacba4572f91e8479de5d99e
Your Name <your.email@example.com>
2024-11-12 10:06:56 -0800

    ugghhhhh

--- netlify/functions/FetchVatsimdata/FetchVatsimdata.js
+++ netlify/functions/FetchVatsimdata/FetchVatsimdata.js
@@ -2,7 +2,7 @@ import { schedule } from '@netlify/functions';
 import axios from "axios";
 
 // Define the scheduled function
-export const handler = schedule("@every 5m", async (event, context) => {
+export const handler = schedule('* * * * *', async (event, context) => {
   let config = {
     method: 'get',
     maxBodyLength: Infinity,

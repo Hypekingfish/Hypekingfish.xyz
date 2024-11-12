c58a4fbb91eebc4ab8b5848e1ebaf4291148f9c0
Your Name <your.email@example.com>
2024-11-11 20:59:44 -0800

    h

--- netlify/functions/FetchVatsimdata/FetchVatsimdata.js
+++ netlify/functions/FetchVatsimdata/FetchVatsimdata.js
@@ -3,20 +3,23 @@ import axios from "axios";
 
 export const handler = schedule('* * * * *', async (event) => {
   try {
-  let config = {
-    method: 'get',
-  maxBodyLength: Infinity,
-    url: 'https://api.vatsim.net/v2/members/1630701/stats',
-    headers: { 
-      'Accept': 'application/json'
-    }
-  };
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://api.vatsim.net/v2/members/1630701/stats',
+      headers: { 
+        'Accept': 'application/json'
+      }
+    };
   
-  axios(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}
+    await axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.error("Error in scheduled function:", error);
+  }
+});

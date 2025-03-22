9a87507d549b7e4bcc3d1c2ba3f44a4641cbbca5
Your Name <your.email@example.com>
2025-03-21 22:17:20 -0700

    revert

--- netlify/functions/FetchVatsimdata/FetchVatsimdata.js
+++ netlify/functions/FetchVatsimdata/FetchVatsimdata.js
@@ -1,42 +1,35 @@
 import { schedule } from '@netlify/functions';
 import fetch from 'node-fetch';
 
-const handler = schedule("* * * * *", async (event, context) => {
+const handler = schedule("0 * * * *", async (event, context) => {
   try {
     console.log('Function started');
-
-    const responses = await Promise.all([
-      fetch('https://api.vatsim.net/v2/members/1630701/stats', {
-        headers: { 'accept': 'application/json'},
-      }),
-      fetch('https://api.vatsim.net/v2/members/1630701/history', {
-        headers: { 'accept': 'application/json'},
-      }),
-    ]);
-
-    const vatsimNetResponse = responses[0]; // Ensure the first response is used
-    const data = await vatsimNetResponse.json();
-
+    const response = await fetch('https://api.vatsim.net/v2/members/1630701/stats', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    
+    const data = await response.json();
     console.log('Received data:', data);
-
+    
     const result = {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        Callsign: data.callsign || "Unknown",
-        controllerHours: data.atc || 0,
-        pilotHours: data.pilot || 0,
-        s1Hours: data.s1 || 0,
-        s2Hours: data.s2 || 0,
-        s3Hours: data.s3 || 0
+        controllerHours: data.atc,
+        pilotHours: data.pilot,
+        s1Hours: data.s1,
+        s2Hours: data.s2,
+        s3Hours: data.s3
       }),
     };
-
+    
     console.log('Returning:', result);
     return result;
-
+    
   } catch (error) {
     console.error('Error:', error);
     return {
@@ -47,3 +40,5 @@ const handler = schedule("* * * * *", async (event, context) => {
 });
 
 export { handler };
+
+ 
